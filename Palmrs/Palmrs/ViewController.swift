7199afffa283eb4d7e80eb6fc59cc12032d8a256
TamojitPalGit <tamojitpal@Tamojits-MacBook-Pro.local>
2016-04-03 21:38:43 +0530

    Initial commit

---
+++ Palmrs/Palmrs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Palmrs
+//
+//  Created by Tamojit Pal on 4/3/16.
+//  Copyright © 2016 Tamojit Pal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
